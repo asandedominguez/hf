7282b68d910145faab220772168a1f760a555459
dam <asandedominguez@danielcastelao.org>
2026-03-16 14:06:28 +0100

    objeto nave

---
+++ nave.py
@@ -0,0 +1,22 @@
+# Clase que representa una nave en el juego
+class Nave:
+    def __init__(self, nombre, tipo, vida):
+        """
+        Constructor de la clase Nave.
+        
+        Args:
+            nombre (str): Nombre de la nave (string)
+            tipo (str): Nombre del barco (submarino, fragata, portaaviones)
+            vida (int): Tamaño de la nave (número de casillas que ocupa)
+        """
+        pass
+
+    def recibir_disparo(self):
+        """
+        Procesa el impacto de un disparo en la nave.
+        Reduce lA vida de la nave y devuelve el estado (Tocado/Hundido).
+        
+        Returns:
+            str: Estado de la nave tras el disparo ("Tocado", "Hundido", etc.)
+        """
+        return ""
