52eca4ac37f2e075535885b84140f8c6f810d28f
Adrian Sande <asandedominguez@danielcastelao.org>
2026-03-24 14:05:18 +0100

    Comentarios de la clase tablero hechos

--- tablero.py
+++ tablero.py
@@ -1,13 +1,18 @@
+#Importación de las clases Nave y Casilla
 from nave import Nave
 from casilla import Casilla
+
+#Creación de la clase tablero del juego. Esta tiene una lista con todas las casillas y va a gestionar los impactos
 class Tablero:
 
     def __init__(self):
 
+#Valores que puede haber al gestionar un impacto
         self.AGUA = 0
         self.TOCADO = 1
         self.HUNDIDO = 2
 
+#Creación de las naves
         por1 = Nave("Enterprise", "portaaviones", 5)
 
         fra1 = Nave("Bismarck", "fragata", 3)
@@ -18,6 +23,8 @@ class Tablero:
         sub2 = Nave("U-96", "submarino", 1)
         sub3 = Nave("U-505", "submarino", 1)
         sub4 = Nave("U-534", "submarino", 1)
+
+#Tablero con las casillas (10x10)
         self.casillero = [
             [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla()],
 
@@ -40,6 +47,7 @@ class Tablero:
             [Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla(), Casilla()]
         ]
 
+#Colocación de las naves en el tablero
         self.casillero[1][1].nave = por1
         self.casillero[1][2].nave = por1
         self.casillero[1][3].nave = por1
@@ -63,6 +71,7 @@ class Tablero:
         self.casillero[7][6].nave = sub3
         self.casillero[9][5].nave = sub4
 
+#Realiza un disparo a la posición que establezcamos. La x son las filas y y columnas. Devolverá el resultado de la acción
     def comprobar_impacto(self, x, y):
         print(f"Impacto en ({x},{y})")
         return self.casillero[x][y].disparar()
